4cbf37828f66b80aedfc57646148ed027cc5a9e6
Arthur Morgado Teixeira <arthurmorgado7751@gmail.com>
2024-08-03 21:23:03 -0300

    Aprimoramento do código.
    Altera a cor de células INCOMPATÍVEIS.

--- main.py
+++ main.py
@@ -1,25 +1,75 @@
-AB = []
-C  = []
-ABC = []
+from colorama import init, Fore, Style
+from copy import deepcopy
 
-print("-- CONFIRA O CÓDIGO E A DESCRIÇÃO PARA ENTENDER --\n")
-linhas = input(f"-Valores das linhas -\n")
-colunas = input(f"--Valores das colunas\n")
+def verificar_matriz(matriz, tamanho_linha, tamanho_coluna):
+    # True: Matriz funciona
+    # False: f"Matriz incorreta, celula_tal"
+    matriz_copia = deepcopy(matriz)
+    for lin in range(tamanho_linha):
+        for col in range(tamanho_coluna):
+   
+            if ((lin + 1) > tamanho_linha - 1):
+                celula3 = matriz[0][col]
+            else:
+                celula3 = matriz[lin + 1][col]
+                
+            if ((col + 1) > tamanho_coluna - 1):
+                celula2 = matriz[lin][0]
+            else:
+                celula2 = matriz[lin][col + 1]
+                
+            celula = matriz[lin][col]
 
-AB = linhas.split(" ")
-C  = colunas.split(" ")
+            if ((verificar_diferencas(celula, celula2)) or (verificar_diferencas(celula, celula3))):
+                matriz_copia[lin][col] = f"{Fore.RED}{celula}{Style.RESET_ALL}"
+            else:
+                matriz_copia[lin][col] = f"{Fore.WHITE}{celula}{Style.RESET_ALL}"
+    
+    for i in matriz_copia:
+        print(f"[{', '.join(i)}]")
+        
 
-m = len(AB)
-n = len(C)
+def verificar_diferencas(celula, celula2):
+    # True: celula != celula2
+    # False: celula == celula
+    diferencas = 0
+    for i in range(len(celula)):
+        if celula[i] != celula2[i]:
+            diferencas += 1
+            if diferencas > 1:
+                return True
+    return False
 
-for i in range(m):
-    ABC.append([])
-    for j in range(n):
-        ABC[i] = [0] * n
+init()
 
-for i in range(m):
-    for j in range(n):
-        ABC[i][j] = AB[i] + C[j]
+print("-- Gerador de Mapa de Karnaught --\n")
 
-for i in ABC:
-    print(i)
+while (True):
+    escolha = int(input("\n[1] USAR\n[0] SAIR: "))
+    
+    if escolha == 0:
+        print("\nEncerrando...")
+        break
+            
+    elif escolha == 1:
+        linhas = input(f"-Valores das linhas -\n")
+        colunas = input(f"--Valores das colunas\n")
+        linha =  []
+        coluna = []
+        matriz = []
+        linha = linhas.split(" ")
+        coluna = colunas.split(" ")
+        tamanho_linha = len(linha)
+        tamanho_coluna = len(coluna)
+        
+        for i in range(tamanho_linha):
+            matriz.append([])
+            for j in range(tamanho_coluna):
+                matriz[i] = [0] * tamanho_coluna
+        for i in range(tamanho_linha):
+            for j in range(tamanho_coluna):
+                matriz[i][j] = linha[i] + coluna[j]
+        print("\n")
+        
+    verificar_matriz(matriz, tamanho_linha, tamanho_coluna)
+    
